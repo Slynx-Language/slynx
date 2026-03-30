91b053dc21a16b0169a0de48557507a9f91d9917
Your Name <alwisgabriel@gmail.com>
2026-03-30 16:27:08 -0300

    fix(checker): use resolve_statments for while body type checking

--- frontend/src/checker/expr.rs
+++ frontend/src/checker/expr.rs
@@ -198,9 +198,7 @@ impl TypeChecker {
                     condition.ty = self.get_type_of_expr(condition)?;
                     let bool_id = self.types_module.bool_id();
                     condition.ty = self.unify(&condition.ty, &bool_id, &condition.span)?;
-                    for stmt in body {
-                        self.default_statement(stmt, &return_type)?;
-                    }
+                    self.resolve_statments(body, ty)?;
                 }
 
                 HirStatementKind::Variable { value, .. } => {
