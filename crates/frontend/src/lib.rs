e6d986d1b85dc904777be541c040b58b43e11386
caina <kina.lima1302@proton.me>
2026-05-02 13:05:28 -0300

    chore: removed frontend crate that was no necessary

--- crates/frontend/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod checker;
-pub mod hir;
-pub mod lexer;
-pub mod monomorphizer;
-pub mod parser;
-
-// expose the primary type directly; users can still refer to the modules
-// through the workspace root if necessary
-pub use checker::TypeChecker;
