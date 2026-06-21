bb5968688a9bcee20912a8ce32c01dfc94a121c9
caina <kina.lima1302@proton.me>
2026-06-21 18:18:41 -0300

    refactor: refactored statements parsing and signatures

--- crates/parser/src/statement.rs
+++ crates/parser/src/statement.rs
@@ -1,27 +1,24 @@
-use crate::{Parser, Result, flags::ParserFlag};
-use slynx_lexer::tokens::{Token, TokenKind};
+use crate::{Parser, Result, Spanned, flags::ParserFlag};
+use slynx_lexer::tokens::TokenKind;
 
-use crate::ast::{ASTStatement, ASTStatementKind};
-use common::Span;
+use crate::ast::ASTStatement;
+use common::{Span, pool::PoolId};
 
-impl Parser {
+impl Parser<'_> {
     ///Parses a let Statement. Until now it's only for variable declaration, so, this only parses 'let name: t = value;' or 'let name = value;', same for mut variants
     ///Maybe, in the future, more things will be parsed.
     ///Obs: this function should initialize right after 'let' token, and the `letstan` is the span of the 'let' token
-    pub fn parse_let_statement(&mut self, letspan: Span) -> Result<ASTStatement> {
+    pub fn parse_let_statement(&mut self, letspan: Span) -> Result<Spanned<PoolId<ASTStatement>>> {
         self.add_flag(ParserFlag::RequireSemicolon);
-        let mut mutable = false;
-        if let TokenKind::Mut = self.peek()?.kind {
+        let mutable = if let TokenKind::Mut = self.peek()?.kind {
             self.eat()?;
-            mutable = true;
-        }
-        let Token {
-            kind: TokenKind::Identifier(name),
-            ..
-        } = self.expect(&TokenKind::Identifier("".into()))?
-        else {
-            unreachable!();
+            true
+        } else {
+            false
         };
+
+        let (name, _) = self.expect_identifier()?;
+
         let vartype = match self.peek()?.kind {
             TokenKind::Colon => {
                 self.eat()?;
@@ -29,47 +26,37 @@ impl Parser {
             }
             _ => None,
         };
-        self.eat()?; //eat '='
+        self.expect(&TokenKind::Eq)?; //eat '='
         let rhs = self.parse_expression()?;
-        Ok(ASTStatement {
-            span: Span {
-                start: letspan.start,
-                end: rhs.span.end,
-            },
-            kind: if mutable {
-                ASTStatementKind::MutableVar {
-                    name,
-                    ty: vartype,
-                    rhs,
-                }
-            } else {
-                ASTStatementKind::Var {
-                    name,
-                    ty: vartype,
-                    rhs,
-                }
-            },
-        })
+        let span = letspan.merge_with(rhs.span);
+        let id = self.intern_statment(if mutable {
+            ASTStatement::MutableVar {
+                name,
+                ty: vartype,
+                rhs,
+            }
+        } else {
+            ASTStatement::Var {
+                name,
+                ty: vartype,
+                rhs,
+            }
+        });
+        Ok(Spanned::new(id, span))
     }
 
-    pub fn parse_while_statement(&mut self, span: Span) -> Result<ASTStatement> {
+    pub fn parse_while_statement(&mut self, span: Span) -> Result<Spanned<PoolId<ASTStatement>>> {
         self.reset_flags();
 
         let condition = self.parse_expression()?;
 
         let (body, block_span) = self.parse_block()?;
 
-        // 3. AST
-        Ok(ASTStatement {
-            span: Span {
-                start: span.start,   // start "while"
-                end: block_span.end, // end "}"
-            },
-            kind: ASTStatementKind::While { condition, body },
-        })
+        let id = self.intern_statment(ASTStatement::While { condition, body });
+        Ok(Spanned::new(id, span.merge_with(block_span)))
     }
 
-    pub fn parse_statement(&mut self) -> Result<ASTStatement> {
+    pub fn parse_statement(&mut self) -> Result<Spanned<PoolId<ASTStatement>>> {
         match self.peek()?.kind {
             TokenKind::Let => {
                 let span = self.eat()?.span;
@@ -84,21 +71,18 @@ impl Parser {
             _ => {
                 let expr = self.parse_expression()?;
                 self.add_flag(ParserFlag::RequireSemicolon);
-                if matches!(self.peek()?.kind, TokenKind::Eq) && expr.is_assignable() {
+                if matches!(self.peek()?.kind, TokenKind::Eq)
+                    && self.expressions.get(expr.data).is_assignable()
+                {
                     self.eat()?;
                     let rhs = self.parse_expression()?;
-                    Ok(ASTStatement {
-                        span: Span {
-                            start: expr.span.start,
-                            end: rhs.span.end,
-                        },
-                        kind: ASTStatementKind::Assign { lhs: expr, rhs },
-                    })
+                    let span = expr.span.merge_with(rhs.span);
+                    let id = self.intern_statment(ASTStatement::Assign { lhs: expr, rhs });
+                    Ok(Spanned::new(id, span))
                 } else {
-                    Ok(ASTStatement {
-                        span: expr.span,
-                        kind: ASTStatementKind::Expression(expr),
-                    })
+                    let span = expr.span;
+                    let id = self.intern_statment(ASTStatement::Expression(expr));
+                    Ok(Spanned::new(id, span))
                 }
             }
         }
