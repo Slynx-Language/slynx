e718528846b5de7a7c45f3b7b813a3416c5e5341
caina <kina.lima1302@proton.me>
2026-07-13 16:02:41 -0300

    feat: added function to handle attribute generation

---
+++ crates/hir/src/builders/attributes/mod.rs
@@ -0,0 +1,41 @@
+use common::Spanned;
+use slynx_parser::ASTAttribute;
+
+use crate::{HirAttribute, HirAttributeKind, SlynxHir, id::AnyDeclarationId};
+
+/// Processes a list of AST attributes and returns their HIR representations.
+///
+/// Known attributes are handled immediately:
+/// - `@builtin("name")` registers the declaration into `LangItems`.
+/// - `@capabilities(...)` is stored for the effect system.
+pub(crate) fn process_attributes(
+    hir: &SlynxHir,
+    attrs: &[Spanned<ASTAttribute>],
+    decl_id: AnyDeclarationId,
+) -> Vec<HirAttribute> {
+    let mut out = Vec::with_capacity(attrs.len());
+    for attr in attrs {
+        let kind = match hir.get_name(attr.data.name) {
+            "builtin" => {
+                let name = attr
+                    .data
+                    .args
+                    .first()
+                    .copied()
+                    .unwrap_or(attr.data.name);
+                hir.lang_items.register(hir.get_name(name), decl_id);
+                HirAttributeKind::Builtin { name }
+            }
+            "capabilities" => HirAttributeKind::Capabilities(attr.data.args.clone()),
+            _ => HirAttributeKind::Unknown {
+                name: attr.data.name,
+                args: attr.data.args.clone(),
+            },
+        };
+        out.push(HirAttribute {
+            kind,
+            span: attr.span,
+        });
+    }
+    out
+}
