b0ac4a87e9e8d6d1440d87151b40a975b845d866
caina <kina.lima1302@proton.me>
2026-08-20 18:04:04 -0300

    tests: added tests for checking reference mutability

--- tests/move_semantics.rs
+++ tests/move_semantics.rs
@@ -1,6 +1,18 @@
+use crate::common::STD_PATH;
+
 mod common;
 #[test]
 fn test_references() {
     let ir = common::compile_ok("examples/move_semantics/references.syx");
     println!("{ir}")
+}
+
+#[test]
+fn test_invalid_writes() {
+    let ir = slynx::compile_to_ir(
+        "examples/move_semantics/invalid_writes.syx".into(),
+        Some(STD_PATH.clone()),
+    );
+    println!("{ir:?}");
+    let _ = ir.expect_err("Code should fail due to invalid writes");
 }
