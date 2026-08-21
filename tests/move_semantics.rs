24474663e223670562b5a1514e9d33a0d71b4af7
caina <kina.lima1302@proton.me>
2026-08-21 17:19:06 -0300

    tests: added tests for borrowing

--- tests/move_semantics.rs
+++ tests/move_semantics.rs
@@ -4,7 +4,7 @@ mod common;
 #[test]
 fn test_references() {
     let ir = common::compile_ok("examples/move_semantics/references.syx");
-    println!("{ir}")
+    println!("References\n{ir}")
 }
 
 #[test]
@@ -13,6 +13,20 @@ fn test_invalid_writes() {
         "examples/move_semantics/invalid_writes.syx".into(),
         Some(STD_PATH.clone()),
     );
-    println!("{ir:?}");
     let _ = ir.expect_err("Code should fail due to invalid writes");
+}
+#[test]
+fn test_valid_writes() {
+    let ir = common::compile_ok("examples/move_semantics/valid_writes.syx");
+    println!("Valid writes\n{ir}")
+}
+
+#[test]
+fn test_invalid_single_writer() {
+    let ir = slynx::compile_to_ir(
+        "examples/move_semantics/invalid_single_writer.syx".into(),
+        Some(STD_PATH.clone()),
+    );
+    let err = ir.expect_err("Code should fail due to invalid single writer");
+    println!("{err}");
 }
